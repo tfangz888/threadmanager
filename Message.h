5d49da31d3a564c3eabc72c05eec701c3ec8cf4b
Tfangz <tfangz@163.com>
2025-12-23 10:33:39 +0800

    Create Message.h

---
+++ Message.h
@@ -0,0 +1,19 @@
+// Message.h
+
+#ifndef MESSAGE_H
+#define MESSAGE_H
+
+#include <string>
+#include <iostream>
+#include <iomanip>
+
+struct Message {
+    int id;
+    std::string data;
+    int priority;  // 0-4 对应5个队列的优先级
+    
+    Message(int _id, const std::string& _data, int _priority = 0)
+        : id(_id), data(_data), priority(_priority) {}
+};
+
+#endif // MESSAGE_H
